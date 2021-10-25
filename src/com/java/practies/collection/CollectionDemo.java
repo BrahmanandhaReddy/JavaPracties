db5d0dde70e6c9808409367a598b7b9f8d164fa3
BrahmanandhaReddy <mbnrswathi143@gmail.com>
2021-10-25 15:56:38 +0530

    add collection demo file

--- src/com/java/practies/collection/CollectionDemo.java
+++ src/com/java/practies/collection/CollectionDemo.java
@@ -1,5 +1,15 @@
 package com.java.practies.collection;
 
-public class CollectionDemo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CollectionDemo {
+public static void main(String[] args) {
+	List<String> ls=new ArrayList<String>();
+	ls.add("Brahma");
+	ls.add("Ramu");
+	ls.add("Ramesh");
+	ls.add("Raghu");
+	System.out.println(ls);
+}
 }
